69f077cefb452b79057c393776198d5edc490849
taky <tkyehab@gmail.com>
2022-06-20 10:08:10 +0200

    follow frontend

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,103 +1,108 @@
-import React,{useState} from "react";
-import TextField from '@mui/material/TextField';
-import { api } from './axios';
-import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableRow from '@mui/material/TableRow';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import { api } from "./axios";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
 import { useHistory } from "react-router-dom";
-import Zoom from '@mui/material/Zoom';
-import { useSelector} from 'react-redux'
-import { useSnackbar } from 'notistack';
-
+import Zoom from "@mui/material/Zoom";
+import { useSelector } from "react-redux";
+import { useSnackbar } from "notistack";
 
 export default function Search(props) {
   const [data, setData] = useState(null);
-	const myInfoState = useSelector(state => state.myInfoState)
-  const { enqueueSnackbar } = useSnackbar()
-  const history = useHistory()
+  const myInfoState = useSelector((state) => state.myInfoState);
+  const { enqueueSnackbar } = useSnackbar();
+  const history = useHistory();
 
   const search = (name) => {
-    api.get(`user/users/${name}/`)
-      .then(users => {
+    api.get(`user/users/${name}/`).then((users) => {
       // console.log(users.data)
-        setData(users.data)
-      })
-  }
+      setData(users.data);
+    });
+  };
   const redirect = (username) => {
-    history.push(`/profile/${username}`)
-    props.closeModel()
-  }
-	const HandelChange = (e) => {
-		if(e.target.value === "") {
-			setData(null)
-		} else {
-			search(e.target.value);
-		}
-	}
-	const follow = (e, user) => {
-    const isF = e.target.textContent
-		e.stopPropagation()
-    api.patch(`user/follow/${user._id}/`, {}, myInfoState.CONFIG).then((info) => {
-      console.log(info.data)
+    history.push(`/profile/${username}`);
+    props.closeModel();
+  };
+  const HandelChange = (e) => {
+    if (e.target.value === "") {
+      setData(null);
+    } else {
+      search(e.target.value);
+    }
+  };
+  const follow = (e, user) => {
+    const isF = e.target.textContent;
+    e.target.textContent = isF === "follow" ? "unfollow" : "follow";
+    e.stopPropagation();
+    api
+      .patch(`user/follow/${user._id}/`, {}, myInfoState.CONFIG)
+      .then((info) => {
+        enqueueSnackbar(`You ${info.data.operation} ${info.data.user}!`, {
+          variant: "info",
+        });
+      })
+      .catch((err) => {
+        enqueueSnackbar(`${err.message}!`, { variant: "error" });
+      });
+  };
 
-      // enqueueSnackbar(isF === 'follow' ? `You FOLLOWED ${user.username}!`:`You UNFOLLOWED ${user.username}!`, { variant: 'info' });
-      // e.target.textContent = isF === 'follow' ? 'unfollow':'follow'
-    }).catch(err => {
-      console.log(err)
-    })
-	}
-  
   return (
     <div>
-        <TextField
-            id="outlined-multiline-static"
-            label="Type Username..."
-            autoComplete="off"
-            rows={3}
-            style={{ width: '100%', marginTop: '2%' }}
-            onChange={HandelChange}
+      <TextField
+        id="outlined-multiline-static"
+        label="Type Username..."
+        autoComplete="off"
+        rows={3}
+        style={{ width: "100%", marginTop: "2%" }}
+        onChange={HandelChange}
+      />
+      <Zoom in={data !== null}>
+        <Paper sx={{ overflow: "hidden" }}>
+          <TableContainer sx={{ maxHeight: 250 }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableBody>
+                {data &&
+                  data.map((user) => {
+                    return (
+                      <TableRow
+                        onClick={() => redirect(user.username)}
+                        hover
+                        role="checkbox"
+                        tabIndex={-1}
+                        key={user._id}
+                      >
+                        <TableCell>{user.username}</TableCell>
 
-        />
-    <Zoom in={data !== null}>
-
-    <Paper sx={{ overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 250 }}>
-        <Table stickyHeader aria-label="sticky table">
-
-          <TableBody>
-            {data && 
-            data.map((user) => {
-                return (
-                  <TableRow onClick={() => redirect(user.username)} hover role="checkbox" tabIndex={-1} key={user._id}>
-                    <TableCell >
-                        {user.username}
-                    </TableCell>
-										
-										{(myInfoState.myInfo && myInfoState.myInfo._id !== user._id) &&
-
-											<TableCell align='right'>
-                        <Button onClick={(e) => follow(e, user)} color="secondary" variant="outlined" > 
-                        
-                           {myInfoState.myInfo.following.filter(e => e.following_user_id === user.username).length > 0 
-                          ? 'Unfollow'
-                          : 'Follow'
-                          }
-                          </Button>
-                    	</TableCell>
-										}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Paper>
-    </Zoom>
-
+                        {myInfoState.myInfo &&
+                          myInfoState.myInfo._id !== user._id && (
+                            <TableCell align="right">
+                              <Button
+                                onClick={(e) => follow(e, user)}
+                                color="secondary"
+                                variant="outlined"
+                              >
+                                {myInfoState.myInfo.following.filter(
+                                  (following) => following.user === user.username
+                                ).length > 0
+                                  ? "Unfollow"
+                                  : "Follow"}
+                              </Button>
+                            </TableCell>
+                          )}
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+      </Zoom>
     </div>
   );
 }
